4bc02518fd6b20d0acb283881044b646101116ca
Persabo <juliobernardotpc@gmail.com>
2024-08-06 10:11:43 -0300

    PersonBehaviour Start

--- Game-Jam-2024/Assets/PersonBehaviour.cs
+++ Game-Jam-2024/Assets/PersonBehaviour.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PersonBehaviour : MonoBehaviour
+[System.Serializable]
+public class PersonBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string name;
+    public bool goodBehaviour;
+    public string setence;
+    public Sprite personFace;
 }
