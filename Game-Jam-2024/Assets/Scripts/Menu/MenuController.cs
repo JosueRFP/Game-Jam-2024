0472156fca7e87c5f93847f5c8ad030ccf7a9b81
Persabo <juliobernardotpc@gmail.com>
2024-08-08 11:37:06 -0300

    Novo Menu

--- Game-Jam-2024/Assets/Scripts/Menu/MenuController.cs
+++ Game-Jam-2024/Assets/Scripts/Menu/MenuController.cs
@@ -4,7 +4,28 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     public GameObject creditsPainel;
+    public Transform normalCamTransform;
+    public Transform newCamTransform;
 
+    public void SetCamPos(bool newPos) 
+    {
+        var cam = Camera.main;
+        if (newPos) 
+        {
+            cam.transform.position = new Vector3(newCamTransform.position.x,newCamTransform.position.y, newCamTransform.position.z);
+            cam.transform.rotation = new Quaternion (newCamTransform.rotation.x,newCamTransform.rotation.y, newCamTransform.rotation.z, 0);
+
+            cam.GetComponent<CamerMove>().enabled = false;
+        }
+        else 
+        { 
+            cam.transform.position = new Vector3(normalCamTransform.position.x, normalCamTransform.position.y, normalCamTransform.position.z);
+
+            cam.transform.rotation = new Quaternion (normalCamTransform.rotation.x, normalCamTransform.rotation.y,normalCamTransform.rotation.z,normalCamTransform.rotation.w);
+
+            cam.GetComponent<CamerMove>().enabled = true;
+        }
+    }
     
     public void Teleport(string tp)
     {
